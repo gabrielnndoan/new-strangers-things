612216769bb11a774d2031b30f8e3937c70847a5
gabrielnndoan <gabrielnndoan@gmail.com>
2021-01-27 20:41:33 -0600

    set up app

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import Navigation from "./components";
+
+const Home = () => {
+  return (
+    <>
+      <Navigation />
+      <div>Welcome to the Homepage</div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import ReactDOM from "react-dom";
+// import {
+//     Navigation
+//   } from './components';
+// import {
+//     Home
+// } from './components/pages'
+
+
+const App = () => {
+    return (
+        // <>
+        // <Navigation />
+        // <Home/>
+        // </>
+        <div>Hello World</div>
+    )
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
