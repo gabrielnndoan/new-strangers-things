9d1a25b2f66c340a889e48d009f4561491a204e4
gabrielnndoan <gabrielnndoan@gmail.com>
2021-01-29 16:51:56 -0600

    posts page and styling

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,30 @@
-import { useHistory } from 'react-router-dom'
-import './Posts.css'
+import { useHistory } from "react-router-dom";
+import "./Posts.css";
 
 const Posts = () => {
-    return (
-        <div className="postPage">
-            <section className="allPosts"> 
-                <h2> All Posts </h2>
-                <section className="post">
+  return (
+    <div className="postPage">
+      <section className="allPosts">
+        <h2> All Posts </h2>
+        <section className="post"></section>
+      </section>
+      <section className="makePost">
+        <h3> Make a New Post </h3>
+        
+          <form className="postForm">
+            <label className="titleLabel" id="wrapper">Item:</label>
+            <input className="titleInput"></input>
 
-                </section>
-            </section>
-            <section className="makePost">
-                <h3> Make a New Post </h3>
-                <button className="makePostButton"> Make a New Post </button>
-            </section>
-        </div>
-    )
-}
+            <label className="descriptionLabel">Description:</label>
+            <input className="descriptionInput"></input>
+
+            <label className="priceLabel">Price:</label>
+            <input className="priceInput"></input>
+          </form>
+        <button className="makePostButton"> Make a New Post </button>
+      </section>
+    </div>
+  );
+};
 
 export default Posts;
